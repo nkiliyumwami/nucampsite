c076edf47d4c84e378f9071311fcb32782a34b16
Emmanuel NKIRIYUMWAMI <nkiliyumwami@gmail.com>
2021-03-14 17:59:07 -0400

    Week 4 Assignment

--- js/script.js
+++ js/script.js
@@ -34,3 +34,16 @@
             });
         });
     
+        //Modal toggle js
+        //Login Modal and Reserve Camp Modal
+        $(function () {
+            $('#loginButton').click(function () {
+                $('#loginModal').modal('show');
+            });
+
+            $('#reserveButton').click(function () {
+                $('#reserveModal').modal('show');
+            });
+        });
+
+
